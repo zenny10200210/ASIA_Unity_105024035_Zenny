66e00591130214ad6a3c7e5d98b5e066587cb5c7
Zenny1020 <zennyzennygood@gmail.com>
2020-01-05 18:27:29 +0800

    怪獸吃壽司

---
+++ Unity_105024035_Zenny/Assets/CamaraScr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraScr : MonoBehaviour
+{
+    public Transform  cam, target;
+    public int speed = 3;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Vector3.Lerp(cam.position, target.position, 0.5f * Time.deltaTime * speed);
+        cam.position = pos;
+    }
+}
---
+++ Unity_105024035_Zenny/Assets/Movement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("移動速度")]
+    [Range(1, 2000)]
+    public int speed = 10;
+    [Header("旋轉速度"), Tooltip("精靈旋轉速度"), Range(1.5f, 200f)]
+    public float turn = 20.5f;
+    [Header("撿東西位置")]
+    public Rigidbody rigCatch;
+
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+    
+    void Update()
+    {
+        Turn();
+        Run();
+        Attack();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(other.name);
+        if (other.name == "壽司" && ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊"))
+        {
+            Physics.IgnoreCollision(other, GetComponent<Collider>());
+            other.GetComponent<HingeJoint>().connectedBody = rigCatch;
+        }
+    }
+
+    private void Turn() 
+    {
+        float h = Input.GetAxis("Horizontal");
+        tran.Rotate(0, turn*h*Time.deltaTime, 0);
+    }
+    private void Run()
+    {
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("攻擊")) return;
+        float v = Input.GetAxis("Vertical");
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
+        ani.SetBool("走路開關",v != 0);
+    }
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ani.SetTrigger("攻擊觸發器");
+        }
+    }
+}
